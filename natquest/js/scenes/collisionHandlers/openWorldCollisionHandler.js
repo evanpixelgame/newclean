6a3688e4aa283b809f995159d18e166e1f696b27
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-10 01:09:32 -0400

    Initial commit

---
+++ natquest/js/scenes/collisionHandlers/openWorldCollisionHandler.js
@@ -0,0 +1,92 @@
+//Creates collision objects, sensor objects, and covers handling of the sensor object interactions with player
+//Import as needed into scenes that have 2.5D maps that require collision/sensor object creation and handling
+//Make sure that all imported maps have their Object layer that deals with collisions is named "Collision Layer 1"
+//Also ensure their Object layer that deals with sensors is named "Sensor Layer 1" and when adding custom properties in Tiled editor, do "customID" for property and then give it a value of whatever label will coorelate with its caused effect 
+
+
+// TRY TO ADD A IF LOGIC TO IT SO THAT IT CAN USE OBJECT PROPERTIES TO DETERMINE WHETHER TO HANDLE THE SENSOR AS ON COLLISION OR OVERLAP
+//expirment with different ways to initiate the callback other than collisionStart and overlap, learn how to use overlap better
+//maybe split up the sensor handling if the switch cases start to get excessive, maybe each scene can get their own sensorhandler if it starts getting excession
+//^^and then this file could just for senor and collision object creation, and then each scene would get its own collision handler function.
+//^^maybe keep the main logic for the sensor handling here and then have the individual sensorhandler file for each scene just handle the different switch cases
+
+export function sensorHandler(scene, map, player, transitionSensors) {
+
+  player.scene.matter.world.on('collisionstart', (eventData) => {
+    // Loop through pairs of colliding bodies
+    eventData.pairs.forEach(pair => {
+      // Check if the player is one of the bodies involved in the collision
+      if (pair.bodyA === player.body || pair.bodyB === player.body) {
+        // Get the other body involved in the collision
+        const otherBody = pair.bodyA === player.body ? pair.bodyB : pair.bodyA;
+        // const isCustom = otherBody.properties.find(prop => prop.name === 'customID') !== undefined;
+        const isCustom = otherBody.isSensor == true;
+
+        if (isCustom) {
+          switch (otherBody.customID) {
+
+            case 'OpenWorldToInsideRoom':
+              console.log('You hit a transition sensor!');
+              // Perform actions specific to this sensor
+              console.log('youve hit the sensor by the door');
+              //scene.scene.remove('ComputerControls');
+              scene.scene.start('NewScene', {
+                player: scene.player,
+                speed: scene.speed,
+                camera: scene.cameras.main,
+                controls: scene.controls, // Passing the controls object here
+                engine: scene.matter.world,
+                world: scene.world,
+              });
+              break;
+
+            case 'fastZone':
+              console.log('cue sirens, double speed');
+              //   scene.speed /= 2;
+              //player.setVelocity(player.velocity.x * 2, player.velocity.y * 2);
+              Matter.Body.setVelocity(scene.player.body, { x: scene.player.body.velocity.x * 2, y: scene.player.body.velocity.y });
+              break;
+
+            case 'BackToOpenWorld':
+              console.log('take me back home daddy');
+              const newPosition = { x: 560, y: 715 };
+              scene.scene.start('OpenWorld', {
+                player: scene.player,
+                speed: scene.speed,
+                camera: scene.cameras.main,
+                controls: scene.controls, // Passing the controls object here
+                engine: scene.matter.world,
+                world: scene.world,
+                newPosition: newPosition,
+              });
+              break;
+
+
+            case 'InsideRoomToNextRoom':
+              console.log('take me back home again daddy');
+              //   const newPosition = { x: 560, y: 715 };
+              //  scene.scene.remove('ComputerControls');
+              scene.scene.start('NextRoom', {
+                player: scene.player,
+                speed: scene.speed,
+                //     camera: scene.cameras.main,
+                controls: scene.controls, // Passing the controls object here
+                //  engine: scene.matter.world,
+                //    world: scene.world,
+                //newPosition: newPosition,
+              });
+              break;
+
+            // Add more cases for other sensor names as needed
+            default:
+              console.log(otherBody.customID);
+              // Handle other sensor names
+              break;
+          }
+        } else {
+          console.log('Collision detected with non-sensor object ID:', otherBody.id);
+        }
+      }
+    });
+  });
+}
---
+++ natquest/js/scenes/scenes/OpenWorld.js
@@ -0,0 +1,96 @@
+import { PlayerSprite } from '../PlayerSprite.js';
+import { sensorMapSet, createCollisionObjects } from '../collisionHandlers/mapSetter.js';
+import { sensorHandler } from '../collisionHandlers/openWorldCollisionHandler.js';
+
+export default class OpenWorld extends Phaser.Scene {
+  constructor() {
+    super({ key: 'OpenWorld' });
+
+    this.map = null;
+    this.player = null;
+    this.engine = null;
+    this.world = null;
+  }
+
+  init(data) {
+    this.openWorldScene = data.OpenWorld;
+    this.player = data.player;
+  }
+
+  preload() {
+  }
+
+  create() {
+    // Create Matter.js engine
+    this.matterEngine = Phaser.Physics.Matter.Matter.World;
+    this.engine = this.matter.world;
+    this.world = this.matterEngine.create({
+      // your Matter.js world options here
+    });
+
+    if (this.sys.game.device.os.android || this.sys.game.device.os.iOS) {
+      this.scene.launch('MobileControls', { player: this.player, speed: this.speed });
+    }
+    this.scene.launch('GameUI', { gameScene: this });
+    this.scene.launch('PlayerAnimations', { player: this.player, speed: this.speed });
+
+    const map = this.make.tilemap({ key: 'map' });
+    // Load tileset
+    const tilesetsData = [
+      { name: 'tilesheetTerrain', key: 'tilesheetTerrain' },
+      { name: 'tilesheetInterior', key: 'tilesheetInterior' },
+      { name: 'tilesheetBuildings', key: 'tilesheetBuildings' },
+      { name: 'tilesheetWalls', key: 'tilesheetWalls' },
+      { name: 'tilesheetObjects', key: 'tilesheetObjects' },
+      { name: 'tilesheetFlourishes', key: 'tilesheetFlourishes' }
+    ];
+
+    const tilesets = [];
+    tilesetsData.forEach(tilesetData => {
+      tilesets.push(map.addTilesetImage(tilesetData.name, tilesetData.key));
+    });
+
+    // Create layers using all tilesets
+    const layers = [];
+    for (let i = 0; i < map.layers.length; i++) {
+      layers.push(map.createLayer(i, tilesets, 0, 0));
+    }
+
+    this.player = new PlayerSprite(this, 495, 325, 'player'); // Create the player object, just took away this.world as 2nd argument
+    console.log(this.player);
+
+    this.scene.launch('PlayerControls', { player: this.player });
+
+    // Set world bounds for the player
+    const boundaryOffset = 2; // increase value to decrease how close player can get to map edge
+    const worldBounds = new Phaser.Geom.Rectangle(
+      boundaryOffset,
+      boundaryOffset,
+      map.widthInPixels - 2 * boundaryOffset,
+      map.heightInPixels - 2 * boundaryOffset
+    );
+
+    this.matter.world.setBounds(0, 0, worldBounds.width, worldBounds.height);
+    console.log(this.world);
+
+    this.collisionObjects = createCollisionObjects(this, map);
+    this.sensorMapping = sensorMapSet(this, map, this.sensorID);
+    this.sensorHandling = sensorHandler(this, map, this.player);
+
+    // Constrain the camera
+    this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+    this.cameras.main.startFollow(this.player, true, 0.05, 0.05);
+    this.cameras.main.setZoom(2);
+  }
+
+  update(time, delta) {
+    // Get player's position and velocity
+    let posX = this.player.body.position.x;
+    let posY = this.player.body.position.y;
+    let velX = this.player.body.velocity.x;
+    let velY = this.player.body.velocity.y;
+    //console.log(posX, posY, velX, velY);
+  }
+
+}
+window.OpenWorld = OpenWorld;
